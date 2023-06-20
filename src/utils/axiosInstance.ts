abec6cc7e65932890847413a4c6cd04c89645ffd
hong <bkn367@naver.com>
2023-06-20 20:43:19 +0900

    now

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:4000/api",
-  baseURL: "http://ec2-52-78-175-136.ap-northeast-2.compute.amazonaws.com/api",
+  baseURL: "http://52.78.175.136/api",
   withCredentials: true,
 });
 
