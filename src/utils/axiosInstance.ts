543ef218c16a1cb9803410da21ebfcaf255205fe
hong <bkn367@naver.com>
2023-07-07 23:48:20 +0900

    test: 배포 테스트 , aws + vercel 배포 테스트 , Fixes:1

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://chanhong.site/api",
   // baseURL: "http://52.78.175.136/api",
   withCredentials: true,
 });
