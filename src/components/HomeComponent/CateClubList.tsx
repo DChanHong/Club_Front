c7af76376f6a675940142068d90a4c25a01bad5c
hong <bkn367@naver.com>
2023-07-14 14:52:25 +0900

    refactor : 불필요 컴포넌트 제거 , 라리브러리 테스트를 위해 만든 임시 컴포넌트 제거(SkelTest.tsx , Loading.tsx) 및 불필요 주석 제거 , Resolves

--- src/components/HomeComponent/CateClubList.tsx
+++ src/components/HomeComponent/CateClubList.tsx
@@ -149,52 +149,6 @@ const CateClubList = ({ data }: { data: string }) => {
             ))}
           </>
         )}
-        {/* {cateClub?.map((item) => (
-          <div
-            key={item.C_IDX}
-            className="
-                flex flex-start 
-                border-4  rounded-3xl mr-4
-                border-slate-200 my-2 w-[26rem]"
-          >
-            <div className="m-3 w-[8rem] ">
-              <Image
-                className="w-[7rem] h-[6rem] border-2 rounded-xl"
-                src={`http://localhost:4000/api/image/background/${item?.C_IMAGE}`}
-                alt={`${item?.U_IDX}`}
-                width={100}
-                height={100}
-                unoptimized={true}
-              />
-            </div>
-            <div>
-              <div className="mt-3 ">
-                <p className="text-[17px]">{item.C_NAME}</p>
-              </div>
-              <div>
-                <p className="text-[#946CEE] underline mb-1">
-                  #{item.C_CATEGORY} #{item.C_CATE_DETAIL} #{item.C_AREA}
-                </p>
-              </div>
-              <div>
-                <p className="text-slate-500 text-[12px] mb-1">
-                  {item.C_INTRO}
-                </p>
-              </div>
-              <div>
-                <button
-                  className="  mb-2 "
-                  type="button"
-                  onClick={() => clubRouterButton(String(item?.C_IDX))}
-                >
-                  <p className="bg-[#946CEE] border-2 rounded-xl text-white p-1 text-[12px]">
-                    입장하기
-                  </p>
-                </button>
-              </div>
-            </div>
-          </div>
-        ))} */}
       </div>
     </div>
   );
