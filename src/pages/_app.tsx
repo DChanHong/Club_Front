9d423ce4b02bef16e9ef9b21b91e1a812b922d90
hong <bkn367@naver.com>
2023-07-14 22:23:29 +0900

    refactor : webfont 수정 , app.tsx에 Inter laing 추가 ,Fixes :2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,26 +19,26 @@ export default function App({ Component, pageProps }: AppProps) {
   // If loading a variable font, you don't need to specify the font weight
   const inter = Inter({ subsets: ["latin"] });
   return (
-    <Provider store={store}>
-      {/* persistGate 컴포넌트는 Redux store가 영구적으로 저장되고 애플리케이션이 새로고침될 떄를 대비해 사용한다.
+    <main className={inter.className}>
+      <Provider store={store}>
+        {/* persistGate 컴포넌트는 Redux store가 영구적으로 저장되고 애플리케이션이 새로고침될 떄를 대비해 사용한다.
           이 컴포넌트는 영구 저장소에서 데이터를 다시 가져온 후에 다른 컴포넌트를 렌더링하므로, 앱이 상태를 복원할 때까지 대기한다. 
           loading prop에는 렌더링 되기 전에 보여질 로딩 컴포넌트를 설정할 수 있으며, 여기서는 null로 설정되어 로딩 컴포넌트를 사용하지 않는다.
           persistor prop에는 앞서 생성한 persistor 객체를 전달한다.
            */}
-      <PersistGate loading={null} persistor={persistor}>
-        <Head>
-          <title>Gathering Site</title>
-        </Head>
-        <div className=" max-w-[2520px] mx-auto">
-          <Header />
-        </div>
+        <PersistGate loading={null} persistor={persistor}>
+          <Head>
+            <title>Gathering Site</title>
+          </Head>
+          <div className=" max-w-[2520px] mx-auto">
+            <Header />
+          </div>
 
-        <QueryClientProvider client={queryClient}>
-          <main className={inter.className}>
+          <QueryClientProvider client={queryClient}>
             <Component {...pageProps} />
-          </main>
-        </QueryClientProvider>
-      </PersistGate>
-    </Provider>
+          </QueryClientProvider>
+        </PersistGate>
+      </Provider>
+    </main>
   );
 }
