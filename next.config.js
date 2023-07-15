19274c829bf4fe088aa7b07d060485ca1ce69472
hong <bkn367@naver.com>
2023-07-15 23:58:16 +0900

    refactor : webfont 에러 수정중 , next.config 수정 , Fixes 8

--- next.config.js
+++ next.config.js
@@ -1,5 +1,32 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     // formats: ["image/avif", "image/webp"],
+//     domains: ["chanhong.site", "api.chanhong.site", "localhost"],
+//     remotePatterns: [
+//       {
+//         protocol: "http",
+//         hostname: "localhost",
+//         protocol: "https",
+//         hostname: "my-club-bucket.s3.ap-northeast-2.amazonaws.com",
+//       },
+//     ],
+//   },
+// };
+
+// const withImages = require("next-images");
+// module.exports = withImages({
+//   webpack(config, options) {
+//     return config;
+//   },
+// });
+
+// module.exports = nextConfig;
+
+const withImages = require("next-images");
+
+module.exports = {
   reactStrictMode: true,
   images: {
     // formats: ["image/avif", "image/webp"],
@@ -13,13 +40,40 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        //ttf, woff, woff2, eot등 여러 확장자일 경우 다음과 같이 추가 가능
+        source: "/.(woff|woff2|ttf|eot)$/i",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Origin, X-Requested-With, Content-Type, Accept",
+          },
+          {
+            key: "Content-Type",
+            value: "application/font-woff",
+          },
+          {
+            key: "Content-Type",
+            value: "application/font-ttf",
+          },
+          {
+            key: "Content-Type",
+            value: "application/octet-stream",
+          },
+        ],
+      },
+    ];
+  },
 };
 
-const withImages = require("next-images");
-module.exports = withImages({
-  webpack(config, options) {
-    return config;
-  },
-});
-
-module.exports = nextConfig;
+module.exports = withImages(module.exports);
