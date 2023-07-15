bd2eb9abf4f34f8b15ad1d6e14b31b09a701b9cf
hong <bkn367@naver.com>
2023-07-16 01:25:18 +0900

    fix : cache 메세지 오류 수정 , getServerSideProps 넣어보기 시도 ,Fixes :3

--- next.config.js
+++ next.config.js
@@ -1,30 +1,4 @@
-import withImages from "next-images";
-
-/** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     // formats: ["image/avif", "image/webp"],
-//     domains: ["chanhong.site", "api.chanhong.site", "localhost"],
-//     remotePatterns: [
-//       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         protocol: "https",
-//         hostname: "my-club-bucket.s3.ap-northeast-2.amazonaws.com",
-//       },
-//     ],
-//   },
-// };
-
-// const withImages = require("next-images");
-// module.exports = withImages({
-//   webpack(config, options) {
-//     return config;
-//   },
-// });
-
-// module.exports = nextConfig;
+const withImages = require("next-images");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -60,3 +34,29 @@ const nextConfig = {
 };
 
 module.exports = withImages(nextConfig);
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     // formats: ["image/avif", "image/webp"],
+//     domains: ["chanhong.site", "api.chanhong.site", "localhost"],
+//     remotePatterns: [
+//       {
+//         protocol: "http",
+//         hostname: "localhost",
+//         protocol: "https",
+//         hostname: "my-club-bucket.s3.ap-northeast-2.amazonaws.com",
+//       },
+//     ],
+//   },
+// };
+
+// const withImages = require("next-images");
+// module.exports = withImages({
+//   webpack(config, options) {
+//     return config;
+//   },
+// });
+
+// module.exports = nextConfig;
